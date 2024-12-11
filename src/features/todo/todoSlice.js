99ccac1d69e29df4f6abf4093ff4efbda2e438d3
Himanshu-Lilhore <himanshulilhore@gmail.com>
2024-12-12 04:18:21 +0530

    first boilerplate

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{id: 1, text: "hello world !!!"}]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const newTodo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(newTodo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter(task => task.id !== action.payload)
+        }
+    }
+})
+
+
+export const {addTodo, removeTodo} = todoSlice.actions
+export default todoSlice.reducer
